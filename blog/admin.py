e86216a078ef7b5cb5be8cac9bbe07170ae6e913
jeremy <sosolinux@163.com>
2016-01-06 11:31:52 +0800

    beautify admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from blog.models import Post,Links,Category
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title','published_date');
+	search_fields = ('title','context');
 
-admin.site.register(Post);
+admin.site.register(Post,PostAdmin);
 admin.site.register(Links);
 admin.site.register(Category);
